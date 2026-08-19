42ce708e3d94c20e9e9fb29bd362f84befbba6ee
Ren Davis <rcdavisgd@gmail.com>
2026-08-19 13:22:08 -0400

    Added CLI11 project

--- AssetBuilder/src/Main.cpp
+++ AssetBuilder/src/Main.cpp
@@ -1,7 +1,10 @@
 
-#include <iostream>
+#include "CLI/CLI.hpp"
 
 int main(int argc, char** argv) {
-	std::cout << "Asset Builder" << std::endl;
+	CLI::App app("Asset Builder");
+	app.add_flag("--verbose", "Enable verbose output");
+	CLI11_PARSE(app, argc, argv);
+
 	return 0;
 }
