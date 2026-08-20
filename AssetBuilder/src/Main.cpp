c82d23fb17eb3b043794d10b23af9b00e042a7da
Ren Davis <rcdavisgd@gmail.com>
2026-08-20 01:11:12 -0400

    Add tilemap parsing options

--- AssetBuilder/src/Main.cpp
+++ AssetBuilder/src/Main.cpp
@@ -1,11 +1,30 @@
 
 #include "CLI/CLI.hpp"
 
+static void ConvertTilemap(const std::string& tilemapPath, const std::string& tilesetPath, const std::string& outputPath);
+
 int main(int argc, char** argv) {
 	CLI::App app("Program for building and converting game assets");
 	app.set_version_flag("-V,--version", "1.0.0");
 
+	std::string tilemapFile;
+	std::string tilesetFile;
+	std::string outputFile;
+
+	auto* tilemaps = app.add_subcommand("tilemaps", "Convert tilemap assets");
+	tilemaps->add_option("-m,--tilemap", tilemapFile, "Input Tiled tile map (tmx)")->required();
+	tilemaps->add_option("-s,--tileset", tilesetFile, "Input Tiled tile set (tsx)")->required();
+	tilemaps->add_option("-o,--output", outputFile, "Output tile map binary")->required();
+
 	CLI11_PARSE(app, argc, argv);
 
+	if (tilemaps->parsed()) {
+		ConvertTilemap(tilemapFile, tilesetFile, outputFile);
+	}
+
 	return 0;
+}
+
+static void ConvertTilemap(const std::string& tilemapPath, const std::string& tilesetPath, const std::string& outputPath) {
+	std::cout << "Parsing tileset: " << tilesetPath << std::endl;
 }
