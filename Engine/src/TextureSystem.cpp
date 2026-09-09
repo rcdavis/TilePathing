613e66a3f9bb1e5cebc487aea351df053cd0cba3
Ren Davis <rcdavisgd@gmail.com>
2026-09-09 06:40:09 -0400

    Removed unneeded texture count var

--- Engine/src/TextureSystem.cpp
+++ Engine/src/TextureSystem.cpp
@@ -2,6 +2,7 @@
 
 #include "OpenGL/GLTexture.h"
 
+#include "TextureIds.h"
 #include "Utils/Log.h"
 
 #include <cassert>
@@ -9,11 +10,10 @@
 namespace TextureSystem {
 	static_assert((uint32_t)Res::Textures::Id::Count > 0, "Texture count must be greater than 0");
 
-	static constexpr uint32_t TextureCount = (uint32_t)Res::Textures::Id::Count;
-	static GLTexture s_Textures[TextureCount];
+	static GLTexture s_Textures[(uint32_t)Res::Textures::Id::Count];
 
 	bool Init() {
-		for (uint32_t i = 0; i < TextureCount; ++i) {
+		for (uint32_t i = 0; i < (uint32_t)Res::Textures::Id::Count; ++i) {
 			const char* const path = Res::Textures::GetPath((Res::Textures::Id)i);
 			if (!s_Textures[i].LoadFromFile(path)) {
 				LOG_ERROR("Failed to load texture: {}", path);
@@ -25,22 +25,22 @@ namespace TextureSystem {
 	}
 
 	void Shutdown() {
-		for (uint32_t i = 0; i < TextureCount; ++i)
+		for (uint32_t i = 0; i < (uint32_t)Res::Textures::Id::Count; ++i)
 			s_Textures[i].Destroy();
 	}
 
 	uint32_t GetTextureId(Res::Textures::Id id) {
-		assert((uint32_t)id < TextureCount && "Texture ID out of range");
+		assert(id < Res::Textures::Id::Count && "Texture ID out of range");
 		return s_Textures[(uint32_t)id].GetId();
 	}
 
 	void Bind(Res::Textures::Id id, uint32_t slot) {
-		assert((uint32_t)id < TextureCount && "Texture ID out of range");
+		assert(id < Res::Textures::Id::Count && "Texture ID out of range");
 		s_Textures[(uint32_t)id].Bind(slot);
 	}
 
 	void Unbind(Res::Textures::Id id) {
-		assert((uint32_t)id < TextureCount && "Texture ID out of range");
+		assert(id < Res::Textures::Id::Count && "Texture ID out of range");
 		s_Textures[(uint32_t)id].Unbind();
 	}
 }
